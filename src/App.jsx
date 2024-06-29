72df0d725f99464259bd59e1ec079decd36e057e
Kunal Garg <imgargk18@gmail.com>
2024-06-29 08:14:50 +0530

    configured redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+      <div className="min-h-screen flex flex-wrap 
+      content-between bg-gray-400">
+        <div className="w-fill block">
+
+          <Header/>
+
+          <main>
+            {/**<Outlet/> */}
+          </main>
+          <Footer/>
+
+        </div>
+
+      </div>) : null
 }
 
 export default App
